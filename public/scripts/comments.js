659218ee3f143a889c70b6e1775f9b110f22e514
Alessio Della Motta <alessio.dellamotta@gmail.com>
2015-08-29 17:49:09 -0700

    Load comments from server polling it

--- public/scripts/comments.js
+++ public/scripts/comments.js
@@ -46,11 +46,31 @@ var CommentForm = React.createClass({
 
 // Main component
 var CommentBox = React.createClass({
+    getInitialState: function() {
+        return {data: []};
+    },
+    loadCommentsFromServer: function() {
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            cache: false,
+            success: function(data) {
+                this.setState({data: data});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+    componentDidMount: function() {
+        this.loadCommentsFromServer();
+        setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+    },
     render: function() {
         return (
             <div className="commentBox">
                 <h1>Comments</h1>
-                <CommentList data={this.props.data}/>
+                <CommentList data={this.state.data}/>
                 <CommentForm />
             </div>
         );
@@ -58,5 +78,5 @@ var CommentBox = React.createClass({
 });
 
 React.render(
-    <CommentBox data={data}/>, document.getElementById('content')
+    <CommentBox url="comments.json" pollInterval={2000} />, document.getElementById('content')
 );
