8b8648a789fe5fc551b89b1ecf3019008d1dfef1
Alessio Della Motta <alessio.dellamotta@gmail.com>
2015-08-29 17:17:40 -0700

    First initialization

--- public/scripts/comments.js
+++ public/scripts/comments.js
@@ -1,1 +1,14 @@
-comments.js
+// Main component
+var CommentBox = React.createClass({
+    render: function() {
+        return (
+            <div className="commentBox">
+                Hello world! I am a comment box!
+            </div>
+        );
+    }
+});
+
+React.render(
+    <CommentBox />, document.getElementById('content');
+);
